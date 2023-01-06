ca4ffcebbfcbd1e9404423092a32839e641803cd
jerry95 <jerry@pandobiz.com>
2023-01-06 23:47:46 +0900

    commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bd811d2c3ab797ba89c52c132674dacd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '94e615a7-c3b0-45ef-bbfa-69ddc1bea1ba'
+};
